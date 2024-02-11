7c6573f4e00eeb65c082840a81c6fa65ea7c8356
Jacob Morris <jacobmorris@Jacobs-MBP.home>
2024-02-11 15:48:10 -0600

    starting css

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,22 +1,23 @@
 import { NavLink } from "react-router-dom"
+import './css/navbar.css'
 
 const Navbar = () => {
 
     return (
-        <div>
+        <div id="headerWhole">
             <header>
                 <div>
                     <p>Abide Chiropractic</p>
                 </div>
-                <div>
+                <div className="linksNav">
                 <NavLink to='/'>Home</NavLink>
                 <NavLink to='/services'>Services</NavLink>
                 <NavLink to='appointment'>Schedule Appointment</NavLink>
                 </div>
                 <div>
-                <a href="mailto:abide.chiropractic22@gmail.com">
+                {/* <a href="mailto:abide.chiropractic22@gmail.com">
                     <img src={require('./pics/mail-icon-envelope-sign-email-symbol-free-vector.jpg')}/>
-                </a>
+                </a> */}
                 
                 </div>
             </header>
